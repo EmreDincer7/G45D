37a6fc9b1099909eda4b3385393175ab2f5db6b4
emre dinçer <emre_dincer70@windowslive.com>
2021-02-21 23:50:28 +0300

    Add project files.

---
+++ G45D/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G45D
+{
+    public class MyDictionary<Tkey,TValue>
+    {
+        Dictionary<Tkey, TValue> myDictionary;
+        Dictionary<Tkey, TValue> tempDictionary;
+
+
+        public MyDictionary()
+        {
+            myDictionary = new Dictionary<Tkey, TValue>(0);
+
+        }
+
+        public void Add(Tkey key, TValue value)
+        {
+            tempDictionary = myDictionary;
+            myDictionary = new Dictionary<Tkey, TValue>(myDictionary.Count + 1);
+
+            foreach (var item in tempDictionary)
+            {
+                myDictionary.Add(item.Key, item.Value);
+            }
+
+            myDictionary.Add(key, value);
+        }
+
+        public int Length
+        {
+            get
+            {
+                return myDictionary.Count;
+            }
+        }
+
+        public Dictionary<Tkey,TValue> MyDictionaryItems
+        {
+            get
+            {
+                return myDictionary;
+            }
+        }
+    }
+}
---
+++ G45D/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace G45D
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+
+            myDictionary.Add(31, "Emre");
+            myDictionary.Add(9, "Meryem");
+            myDictionary.Add(26, "Gamze");
+            myDictionary.Add(29, "Kerim");
+
+            Console.WriteLine(myDictionary.Length);
+
+            foreach (var item in myDictionary.MyDictionaryItems)
+            {
+                Console.WriteLine(item.Key + " - " + item.Value);
+            }
+
+        }
+    }
+}
